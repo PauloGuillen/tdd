3164abc424e907f770e546b1f1abcb9b85327769
PauloGuillen <psm.guillen@gmail.com>
2023-05-23 09:15:52 -0300

    Teste com um elemento na lista

--- superlists/tests/functional_test.py
+++ superlists/tests/functional_test.py
@@ -26,12 +26,15 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_element(By.TAG_NAME, 'tr').text
-        self.assertTrue(
-            any(row.text == '1: Buy pescoak feathers' for row in rows),
-            'New to-do item did not appear in table')
+        inputbox = self.browser.find_element(By.ID,'id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
 
         self.fail('Finish the test!')
     
